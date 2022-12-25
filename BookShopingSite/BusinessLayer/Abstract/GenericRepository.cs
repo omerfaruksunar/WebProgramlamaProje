ef37e3ece99a1957caa0e1ddeda54f56fee84e07
omerfaruksunar <omerfaruksunar@gmail.com>
2022-12-25 21:54:12 +0300

    BusinessLayer katmani islemleri

---
+++ BookShopingSite/BusinessLayer/Abstract/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLayer.Abstract
+{
+    //IRepository interface sınıfında tanımladıgımız metodlari GenericRepository kullanarak Generic islemlerine yaptıracak fonksiyonları yaziyoruz.
+    public class GenericRepository<T>:IRepository<T> where T : class,new()
+    {
+        //DataContext'e referans alarak ulasiyoruz ve ardından db isminde bir nesne olusturarak kullaniyoruz.
+        DataContext db= new DataContext();
+        DbSet<T> data;
+
+        //Silme islemi icin gerekli fonksiyonu yazip DataBase'deki degisikligi kaydediyoruz.
+        public void Delete(T p) 
+        { 
+            data.Remove(p);
+            db.SaveChanges();
+            
+        }
+        //Ekleme islemi icin gerekli fonksiyonu yazip DataBase'deki degisikligi kaydediyoruz.
+        public void Insert(T p)
+        {
+            data.Add(p);
+            db.SaveChanges();   
+        }
+        //Guncelleme islemi icin gerekli fonksiyonu yazip DataBase'deki degisikligi kaydediyoruz.
+        public void Update(T p)
+        {
+            db.Entry<T>(p).State= EntityState.Modified;
+            db.SaveChanges();
+        }
+        //Database den eleman getirme islemi icin gerekli fonksiyonu yaziyoruz.
+        public T GetById(int id) 
+        {
+            return data.Find(id);   
+        }
+        //Liste getirme islemi icin gerekli fonksiyonu yaziyoruz.
+        public List<T> List()
+        {
+            return data.ToList();
+        }
+    }
+}
---
+++ BookShopingSite/BusinessLayer/Abstract/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    //Generic olarak bir T tanımladık ve bu T'ye class olma ve new'lenebilme özelliklerini atadık.
+    public interface IRepository<T> where T : class,new()
+    {
+        //Listeleme metodunu list olarak tanımladık.
+        List<T> List();
+        //Insert olarak ekleme islemini yapacagimiz metodu tanimladik.
+        void Insert(T p);
+        //Update olarak guncelleme islemini yapacagimiz metodu tanimladik.
+        void Update(T p);
+        //Delete olarak silme islemini yapacagimiz metodu tanimladik.
+        void Delete(T p);
+        //GetById kullanarak id degerimizle eleman çekme islemini gerceklestirecek islemi tanımladık.
+        T GetById(int id);
+    }
+}
